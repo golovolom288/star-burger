51846ae7debf7c1e3d84d6e9945e5ebb349aede6
golovolom288 <142310490+golovolom288@users.noreply.github.com>
2025-11-19 21:40:20 +0600

    cделал проверку response

--- geopy_bd/views.py
+++ geopy_bd/views.py
@@ -13,8 +13,11 @@ def fetch_coordinates(apikey, address):
         "format": "json",
     })
     response.raise_for_status()
-    found_places = response.json()['response']['GeoObjectCollection']['featureMember']
-
+    if response.ok:
+        found_places = response.json()['response']['GeoObjectCollection']['featureMember']
+    else:
+        print("Ссылка на яндекс геокод не работает")
+        return
     if not found_places:
         return None
 
