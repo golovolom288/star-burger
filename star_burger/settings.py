c27c84e2bbe13ed894263e910363f56fff637eda
golovolom288 <git@github.com>
2026-03-01 07:10:51 +0000

    update settings

--- star_burger/settings.py
+++ star_burger/settings.py
@@ -15,7 +15,7 @@ ENVIRONMENT = 'production'
 SECRET_KEY = env('SECRET_KEY')
 DEBUG = env.bool('DEBUG', True)
 YANDEX_API_KEY = env('YANDEX_API_KEY')
-ROLLBAR_TOKEN = env('ROLLBAR_TOKEN')
+ROLLBAR_TOKEN = env.list('ROLLBAR_TOKEN', "")
 
 ALLOWED_HOSTS = env.list('ALLOWED_HOSTS', ['127.0.0.1', 'localhost'])
 
